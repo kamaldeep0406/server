82ee934bafb4ba782fa7678a288e581018d92857
Chakshu1409 <97241238+Chakshu1409@users.noreply.github.com>
2024-06-27 05:44:45 +0530

    ready to deploy

--- database/db.js
+++ database/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
+const DB_USERNAME= "user"
+const DB_PASSWORD="codeforinterview"
+
 const Connection = async (username, password) => {
-    const URL = `mongodb://user:codeforinterview@ac-wlexvdx-shard-00-00.hjbhcgg.mongodb.net:27017,ac-wlexvdx-shard-00-01.hjbhcgg.mongodb.net:27017,ac-wlexvdx-shard-00-02.hjbhcgg.mongodb.net:27017/?replicaSet=atlas-10szrh-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=whatsapp-clone`;
+    const URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ac-wlexvdx-shard-00-00.hjbhcgg.mongodb.net:27017,ac-wlexvdx-shard-00-01.hjbhcgg.mongodb.net:27017,ac-wlexvdx-shard-00-02.hjbhcgg.mongodb.net:27017/?replicaSet=atlas-10szrh-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=whatsapp-clone`;
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
